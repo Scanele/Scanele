0b64476cde30eb22566be7545fea47c41eb507fc
Scanele <2793450606@qq.com>
2020-03-26 21:00:32 +0800

    Site updated: 2020-03-26 21:00:29

--- js/matery.js
+++ js/matery.js
@@ -73,9 +73,8 @@ $(function () {
         $('#articleContent a').attr('target', '_blank');
 
         $('#articleContent img').each(function () {
-            let imgPath = '/'+$(this).attr('src');
-            $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>'); 
-            $(this).attr('src',imgPath);
+            let imgPath = $(this).attr('src');
+            $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
             // 图片添加阴影
             $(this).addClass("img-shadow img-margin");
             // 图片添加字幕
